aa19051e4be9e3a853ebb5e08d8236879468fe3b
Harrison Weinstock <hkweinstock@gmail.com>
2023-12-03 16:00:00 -0500

    implement transformer type

--- src/LuTypeChecker.hs
+++ src/LuTypeChecker.hs
@@ -1,5 +1,6 @@
 module LuTypeChecker where
 
+import Control.Monad
 import LuSyntax
 import State (State)
 import Data.Map (Map)
@@ -9,18 +10,40 @@ import LuTypes
 isValueType :: LType -> Bool 
 isValueType = undefined
 
+newtype EitherT m a = EitherT { runEitherT :: m (Either String a) }
+
+instance Monad m => Monad (EitherT m) where 
+    return :: a -> EitherT m a
+    return = pure 
+    (>>=) = bind where 
+        bind :: EitherT m a -> (a -> EitherT m b) -> EitherT m b
+        bind x f = EitherT $ do 
+            eitherValue <- runEitherT x 
+            case eitherValue of 
+                Left s -> return $ Left s
+                Right v -> runEitherT $ f v
+
+instance Monad m => Applicative (EitherT m) where
+    pure = EitherT . return . Right
+    (<*>) = ap
+
+instance Monad m => Functor (EitherT m) where
+    fmap = liftM
+
 type EnvironmentTypes = Map Name LType
-type EnvironmentState = State EnvironmentTypes ()
+-- type TypecheckerState = EitherT (State EnvironmentTypes) ()  -- transformer version
+
+type TypecheckerState = State EnvironmentTypes (Either String ())
 
 -- | Given a block and an environment, check if the types are consistent in the block. 
-typeCheckBlock :: Block -> EnvironmentState -> State EnvironmentTypes ()
+typeCheckBlock :: Block -> TypecheckerState
 typeCheckBlock = undefined
 
 -- | Given a statement and an environment, check if the types are consistent in the statement. 
-typeCheckStatement :: Statement -> EnvironmentState -> State EnvironmentTypes () 
+typeCheckStatement :: Statement -> TypecheckerState
 typeCheckStatement = undefined
 
--- | Determine type of a given expression and with environment. 
+-- | Determine type of a given expression with environment. 
 synthesis :: Expression -> EnvironmentTypes -> LType
 synthesis = undefined
 
